20dc7725eaa5c4feed237703b08b2f2c7b41a14f
Greyson Murray <greyson.murray@gmail.com>
2020-11-02 09:00:05 -0500

    create dir'

---
+++ prntsc/prntsc.py
@@ -0,0 +1,13 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import lxml
+import os
+import random
+import string
+
+
+def main()
+
+
+if __name__ == "__main__":
+	main()
